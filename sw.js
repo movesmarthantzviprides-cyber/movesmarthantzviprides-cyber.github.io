d2bf812662a64a0bbbdce9db08182603c1f4caa6
movesmarthantzviprides-cyber <movesmarthantzviprides@gmail.com>
2026-08-02 02:28:10 +0100

    Update sw.js
    Add SW js

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
-.
+const CACHE_NAME = 'hantzvip-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request).then(res => res || caches.match('/offline.html'));
+    })
+  );
+});
